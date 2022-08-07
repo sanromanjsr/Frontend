4f3ea65bd7a6bc0f397bed74a27e63e35e9488b5
jonatan <ext_jsanroma@mercadolibre.com>
2022-08-07 18:50:19 -0300

    Creacion de componenentes "sociales" , "banner", "logo-ap".

--- ProyectoPortafolio/src/app/app.module.ts
+++ ProyectoPortafolio/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    RedesSocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
