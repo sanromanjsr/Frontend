3df498d7d2013565e3c2786e8fe13e58739a37a7
jonatan <ext_jsanroma@mercadolibre.com>
2023-01-12 11:06:55 -0300

    Se actualiza la url

--- src/app/service/skill.service.ts
+++ src/app/service/skill.service.ts
@@ -1,14 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 import { Skill } from '../model/skill';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SkillService {
-  URL = environment.URL + 'skill/';
+  URL = environment.URL + '/skill/';
   
   constructor(private httpClient: HttpClient) { }
 
